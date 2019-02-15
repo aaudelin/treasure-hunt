ed2cbdef69eae3a51c9c2857889114a2c6b85b2e
aaudelin <aaudelin@tradelab.fr>
2019-02-15 10:50:22 +0100

    Init madre de dios map test

--- src/test/java/model/entity/field/MadreDeDiosFieldTest.java
+++ src/test/java/model/entity/field/MadreDeDiosFieldTest.java
@@ -1,14 +1,14 @@
 package model.entity.field;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
+
+import helper.factory.FieldFactory;
 
 public class MadreDeDiosFieldTest {
 
 	@Test
 	public void testAdventurerToMountain() {
-		fail("Not yet implemented");
+		FieldFactory.createMadreDeDiosField(5, 5);
 	}
 
 }
